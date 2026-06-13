f4e07a0bb8fb02302347adcc1446b9108eae6a4e
NamNguyen-edu <nhatnam17092005@gmail.com>
2026-06-13 18:07:54 +0700

    ML2 - Regression Profit Prediction

--- src/config/cluster_config.py
+++ src/config/cluster_config.py
@@ -13,3 +13,13 @@ DEFAULT_SHUFFLE_PARTITIONS = 8
 DEFAULT_PARALLELISM = 8
 
 APP_NAME = "Nhom10_BigData"
+
+# Kafka Config
+KAFKA_HOME = r"D:\java\kafka_2.13-4.1.2"
+KAFKA_BOOTSTRAP_SERVER = "master:9092"
+INPUT_TOPIC = "profit_prediction_requests"
+OUTPUT_TOPIC = "profit_prediction_results"
+
+
+# Streaming Checkpoint
+CHECKPOINT_PATH = "hdfs://master:9000/checkpoints/profit_stream"
